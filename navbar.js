1742d0a86d15b7d1adacf0f1e4aed4644e148a16
Shiva-fighter <shiva.kashyap0008@gmail.com>
2022-01-21 21:58:11 +0530

    create clone of shopbop

---
+++ navbar.js
@@ -0,0 +1,92 @@
+function navbar(){
+    return ` <div class = "nav">
+
+    <div class = "upnav">
+        <div class = "upnav_select">
+            <p>IN</p>
+            <img src = "https://principlebusinessenterprises.com/wp-content/uploads/2018/10/angle_down_18635.png">
+            <p>EN</p>
+            <img src = "https://principlebusinessenterprises.com/wp-content/uploads/2018/10/angle_down_18635.png">
+            <p>$USD</p>
+            <img src = "https://principlebusinessenterprises.com/wp-content/uploads/2018/10/angle_down_18635.png">
+        </div>
+
+        <div class = "upnav_h5">
+             <h5>FREE express international delivery + EASY returns</h5>
+        </div>
+
+        <div  class = "upnav_sign">
+            <ul>
+                <a href="signin.html">Sign In / </a>
+                <a href="signup.html">Register</a>
+                <img src="https://sorted.app/images/icon_love_red.png" alt="">
+                <img src="https://webiconspng.com/wp-content/uploads/2016/11/bag_basket_buy_cart_luggage_shopping_suitcase_icon_1540167.png" alt="">
+            </ul>
+        </div >
+       
+    </div>
+    <div class = "main_logo">
+        <img src="https://mma.prnewswire.com/media/551408/Shopbop_Logo.jpg?p=facebook" alt="">
+        
+    </div>
+  </div>
+  <nav>
+    <div class = "wrapper">
+      <ul class = "nav-links">
+        
+        <li> <a href=""> WHAT'S NEW</a>
+          <div class="mega-box">
+            <div class="content">
+              <div class="row">
+                <img src="https://m.media-amazon.com/images/G/01/Shopbop/media/desktop/topnav/2022/2022_01/sb_20220103_desktop_whatsnew_slot1_FashionForecast.jpg" alt="">
+              </div>
+              <div class="row">
+                <header>WHAT'S NEW</header>
+                <ul class="mega-links">
+                  <li><a href="#">New Today</a></li>
+                  <li><a href="#">New This Week</a></li>
+                  <li><a href="#">Top Sellers</a></li>
+                  <li><a href="#">Designer Boutique</a></li>
+                  <li><a href="#">All What's New</a></li>
+                </ul>
+              </div>
+              <div class="row">
+                <header>WHAT'S NEW BY CATEGORY</header>
+                <ul class="mega-links">
+                  <li><a href="product.html">Clothing</a></li>
+                  <li><a href="#">Shoes</a></li>
+                  <li><a href="#">Bags</a></li>
+                  <li><a href="#">Jewelery & Accessories</a></li>
+                  <li><a href="men.html">Men</a></li>
+                </ul>
+              </div>
+              <div class="row">
+                <header>DISCOVER</header>
+                <ul class="mega-links">
+                  <li><a href="#">Soak Up The Sun With Xirena</a></li>
+                  <li><a href="#">So Dreamy</a></li>
+                  <li><a href="#">Take A Trip With Zimmermann</a></li>
+                  <li><a href="#">Shopbop Moves: The Active Edit</a></li>
+                  <li><a href="#">Just In:FARM Rio</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li> <a href="">OUR FAVORITES</a></li>
+        <li> <a href="">DESIGNERS</a></li>
+        <li> <a href="">CLOTHING</a></li>
+        <li> <a href="">ACTIVE</a></li>
+        <li> <a href="">SHOES</a></li>
+        <li> <a href="">BAGS</a></li>
+        <li> <a href="">ACCESSORIES</a></li>
+        <li> <a href="">MEN</a></li>
+        <li> <a href="">SALE</a></li>
+  
+  
+      </ul>
+    </div>
+  </nav>`
+}
+
+export default navbar
